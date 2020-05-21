3ba9b76c27b9465ecfb6a34c63e64fbbfef148d6
Thomas Chamelot <chamelot.thomas@gmail.com>
2020-05-21 13:20:17 +0200

    Replace hardcoded values in controller

--- pru/controller.c
+++ pru/controller.c
@@ -1,5 +1,6 @@
 #pragma CHECK_MISRA("none")
 #include <stdint.h>
+#include <am335x/pru_ctrl.h>
 #include <am335x/pru_cfg.h>
 #include <am335x/pru_intc.h>
 #include <am335x/pru_ecap.h>
@@ -7,9 +8,6 @@
 #pragma RESET_MISRA("all")
 
 #define PERIOD_NS 10000000U
-
-volatile register uint32_t __R30;
-volatile register uint32_t __R31;
 
 struct pid_t {
     int32_t kp;
@@ -21,6 +19,8 @@ struct controller_t {
     volatile int32_t input;
     volatile int32_t output;
     volatile struct pid_t coef;
+    volatile uint32_t pru0_cycle;
+    volatile uint32_t pru0_stall;
 };
 
 #pragma DATA_SECTION(controller, ".sdata")
@@ -30,15 +30,14 @@ void configure_timer(void);
 void main(void);
 
 void configure_timer(void) {
-    CT_INTC.GER_bit.EN_HINT_ANY = 1U;    /* Enable global interrupt */
-    CT_INTC.CMR3_bit.CH_MAP_15 = 0U;     /* Map S15 to channel 0 */
-    CT_INTC.ESR0 |= (uint32_t)1U << 15;            /* Enable S15 */
+    CT_INTC.CMR3_bit.CH_MAP_15 = 0U;                /* Map S15 to channel 0 */
+    CT_INTC.EISR = PRU_INT_ECAP;                    /* Enable S15 */
     CT_ECAP.CAP3 = (uint32_t)PERIOD_NS / 5U - 1U;   /* Set the sampling period */
-    CT_ECAP.ECCTL2 = (uint16_t)(((uint32_t)1U << 9) | ((uint32_t)1U << 4)); /* APWM mode and counter free-running */
-    CT_ECAP.TSCTR = 0U;                  /* Reset the counter */
-    CT_ECAP.ECEINT = 0x80U;              /* Enable intterupt on CAP3 == TSCTR */
-    CT_ECAP.ECCLR  = 0xffU;              /* Clear interrput flags */
-    CT_INTC.SECR0 = 0xFFFFFFFFU;         /* Clear the status of all interrupts */
+    CT_ECAP.ECCTL2 = ECAP_APWM_MODE | ECAP_CTRRUN;  /* APWM mode and counter free-running */
+    CT_ECAP.TSCTR = 0U;                             /* Reset the counter */
+    CT_ECAP.ECEINT = ECAP_INT_CMPEQ;                /* Enable intterupt on CAP3 == TSCTR */
+    CT_ECAP.ECCLR  = 0xffU;                         /* Clear interrput flags */
+    CT_INTC.SECR0 = 0xFFFFFFFFU;                    /* Clear the status of all interrupts */
 	CT_INTC.SECR1 = 0xFFFFFFFFU;
 }
 
@@ -49,50 +48,75 @@ void main(void) {
     int32_t previous = 0;
     struct pid_t pid;
 
+    /* performance */
+    uint32_t cycle = 0U;
+    uint32_t stall = 0U;
+
     CT_CFG.SYSCFG_bit.STANDBY_INIT = 0U;    /* enable OCP master port */
+    CT_INTC.SECR0 = 0xFFFFFFFFU;            /* Clear the status of all interrupts */
+	CT_INTC.SECR1 = 0xFFFFFFFFU;
 
-    /* notify host */
-    __R31 = 0x23U;
+    notify_mst(MST_3);
 
-    /* wait for host */
-    while((__R31 >> 30) & 1U == 0U) {}
-    CT_INTC.SICR_bit.STS_CLR_IDX = 21U;
+    wait_for_int_host0(PRU_INT_MST_5);
 
     /* store pid coef in local memory */
     pid.kp = controller.coef.kp;
     pid.ki = controller.coef.ki;
     pid.kd = controller.coef.kd;
 
-    for(i = 0U; i < 8000000U; i++) {
-    }
+    /* for(i = 0U; i < 8000000U; i++) {
+    } */
 
     configure_timer();
+
+    /* enable cycle counter */
+    #pragma CHECK_MISRA("none")
+    PRU0_CTRL.CTRL_bit.CTR_EN = 1U;
+    #pragma RESET_MISRA("all")
+
     /* wait timer interrupt */
     for(i = 0U; i < 10U; i++) {
-        while((__R31 >> 30) & 1U == 0U) {}
-        CT_INTC.SICR_bit.STS_CLR_IDX = 15U;  /* Clear PRU interrupt */
-        CT_ECAP.ECCLR = 0xffU;               /* Clear timer interrupt */
+        wait_for_int_host0(PRU_INT_ECAP);
+        CT_ECAP.ECCLR = 0xffU;                          /* Clear timer interrupt */
     
+        #pragma CHECK_MISRA("none")
+        cycle = PRU0_CTRL.CYCLE;
+        stall = PRU0_CTRL.STALL;
+        #pragma RESET_MISRA("all")
+
         err += controller.input;
         diff = controller.input - previous;
         controller.output = (pid.kp * controller.input) +
                             (pid.kd * diff) +
                             (pid.ki * err);
 
-        __R31 = 0x23U;                       /* Send interrupt S16 to host */
+        notify_mst(MST_4);
+
+        #pragma CHECK_MISRA("none")
+        controller.pru0_stall = PRU0_CTRL.STALL - stall;
+        controller.pru0_cycle = PRU0_CTRL.CYCLE - cycle;
+        #pragma RESET_MISRA("all")
     }
 
-    /* notify host */
-    /* __R31 = 0x23; */
-    
-    /* wait for host */
-    while((__R31 >> 30) & 1U == 0U) {}
-    CT_INTC.SICR_bit.STS_CLR_IDX = 21U;
-    for(i = 0U; i < 8000000U; i++) {
-    }
+    wait_for_int_host0(PRU_INT_MST_5);
+    /* for(i = 0U; i < 8000000U; i++) {
+    } */
     
     /* notify host */
-    __R31 = 0x23U;
+    notify_mst(MST_3);
 
     __halt();
+}
+
+
+inline void notify_mst(enum mst_interrupt_t evt) {
+    __R31 = (uint32_t)0x20U | evt;
+}
+
+inline void wait_for_int_host0(uint32_t evt) {
+    do{
+        while((__R31 & 0x40000000U) == 0U) {}
+    }while(CT_INTC.HIPIR0 != evt);
+    CT_INTC.SICR_bit.STS_CLR_IDX = evt;
 }
--- pru/controller.h
+++ pru/controller.h
@@ -4,12 +4,39 @@
 
 /* PRU CONSTANT */
 #define PRU_INT_ECAP    15U
-#define PRU_INT_HOST_3  16U
-#define PRU_INT_HOST_1  21U  
+#define PRU_INT_MST_3   19U
+#define PRU_INT_MST_4   20U  
+#define PRU_INT_MST_5   21U  
 
 #define ECAP_APWM_MODE  ((uint16_t)0x200U)
-#define ECAP_TSCTRSTOP  ((uint16_t)0x10U)
+#define ECAP_CTRRUN     ((uint16_t)0x10U)
 
 #define ECAP_INT_CMPEQ  ((uint16_t)0x80U)
+
+/* PRU utilities */
+volatile register uint32_t __R30;
+volatile register uint32_t __R31;
+
+enum mst_interrupt_t{
+    MST_0 = 0U,
+    MST_1 = 1U,
+    MST_2 = 2U,
+    MST_3 = 3U,
+    MST_4 = 4U,
+    MST_5 = 5U,
+    MST_6 = 6U,
+    MST_7 = 7U,
+    MST_8 = 8U,
+    MST_9 = 9U,
+    MST_10 = 10U,
+    MST_11 = 11U,
+    MST_12 = 12U,
+    MST_13 = 13U,
+    MST_14 = 14U,
+    MST_15 = 15U
+};
+
+inline void notify_mst(enum mst_interrupt_t evt);
+inline void wait_for_int_host0(uint32_t evt);
 
 #endif
