65c451f3b25f52690dbea345357573568aea242f
Thomas Chamelot <chamelot.thomas@gmail.com>
2020-06-06 23:41:03 +0200

    [PRU] control each motor independantly

--- pru/motor.c
+++ pru/motor.c
@@ -8,14 +8,13 @@
 #include "drosix.h"
 #pragma RESET_MISRA("all")
 
-#define PERIOD_NS 20000000U
-
 void main(void);
 void configure_timer(void);
 
 void main(void) {
     uint8_t run = 1U;
-    uint32_t duty_cycles = 
+    uint32_t cycle = 0U;
+    uint32_t duty_cycles[4] = {179999U, 179999U, 179999U, 179999U};
 
     CT_CFG.SYSCFG_bit.STANDBY_INIT = 0U;    /* enable OCP master port */
 
@@ -28,7 +27,7 @@ void main(void) {
     while(run == 1U) {
         switch(check_event1()) {
         case EVT_PWM_STEP:
-			CT_IEP.TMR_CMP_STS = 0x1U;
+            CT_IEP.TMR_CMP_STS = 0x1U;
             send_event(MST_15);
             set_pins(ALL_MOTORS);
             break;
@@ -39,8 +38,10 @@ void main(void) {
         /* New data */
         case EVT_PID_OUTPUT:
             /* send_event(MST_15); */
-            /* handle new data */
-            duty_cycles = controller.outputs;
+            duty_cycles[0] = controller.outputs[0];
+            duty_cycles[1] = controller.outputs[1];
+            duty_cycles[2] = controller.outputs[2];
+            duty_cycles[3] = controller.outputs[3];
             break;
         /* No event yet */
         case None:
@@ -52,8 +53,18 @@ void main(void) {
         }
 
         /* process pwms */
-        if(CT_IEP.TMR_CNT >= duty_cycles) {
-            clear_pins(ALL_MOTORS);
+        cycle = CT_IEP.TMR_CNT;
+        if(cycle >= duty_cycles[0]) {
+            clear_pins(MOTOR_1);
+        }
+        if(cycle >= duty_cycles[1]) {
+            clear_pins(MOTOR_2);
+        }
+        if(cycle >= duty_cycles[2]) {
+            clear_pins(MOTOR_3);
+        }
+        if(cycle >= duty_cycles[3]) {
+            clear_pins(MOTOR_4);
         }
     }
 
@@ -64,18 +75,16 @@ void main(void) {
 }
 
 void configure_timer(void) {
-    CT_INTC.CMR1_bit.CH_MAP_7 = 1U;                 /* Map S7 to channel 0 */
-    CT_INTC.EISR = IEP_TIMER;                       /* Enable S15 */
-	CT_IEP.TMR_GLB_CFG_bit.CNT_EN = 0U;             /* Disable counter */
-	CT_IEP.TMR_CNT = 0xFFFFFFFFU;		            /* Reset Count register */
-	CT_IEP.TMR_GLB_STS_bit.CNT_OVF = 0x1U;          /* Clear overflow status register */
-	CT_IEP.TMR_CMP0 = PERIOD_NS / 5U - 1U;                     /* Set compare0 value */
-	CT_IEP.TMR_CMP_STS_bit.CMP_HIT = 0xFFU;         /* Clear compare status */
-	CT_IEP.TMR_COMPEN_bit.COMPEN_CNT = 0x0U;        /* Disable compensation */
-	CT_IEP.TMR_CMP_CFG_bit.CMP0_RST_CNT_EN = 0x1U;  /* Disable CMP0 and reset on event */
-	CT_IEP.TMR_CMP_CFG_bit.CMP_EN = 0x1U;
-	CT_IEP.TMR_GLB_CFG_bit.DEFAULT_INC = 0x1U;		/* Configure incr value */
-	CT_IEP.TMR_GLB_CFG_bit.CNT_EN = 1U;             /* Enable counter */
-	/* CT_INTC.SECR0 = 0xFFFFFFFFU;
-	CT_INTC.SECR1 = 0xFFFFFFFFU;*/
+    CT_INTC.CMR1_bit.CH_MAP_7 = 1U;                 /* Map S7 to channel 0              */
+    CT_INTC.EISR = IEP_TIMER;                       /* Enable S15                       */
+    CT_IEP.TMR_GLB_CFG_bit.CNT_EN = 0U;             /* Disable counter                  */
+    CT_IEP.TMR_CNT = 0xFFFFFFFFU;                   /* Reset Count register             */
+    CT_IEP.TMR_GLB_STS_bit.CNT_OVF = 0x1U;          /* Clear overflow status register   */
+    CT_IEP.TMR_CMP0 = PWM_PERIOD / 5U - 1U;          /* Set compare0 value               */
+    CT_IEP.TMR_CMP_STS_bit.CMP_HIT = 0xFFU;         /* Clear compare status             */
+    CT_IEP.TMR_COMPEN_bit.COMPEN_CNT = 0x0U;        /* Disable compensation             */
+    CT_IEP.TMR_CMP_CFG_bit.CMP0_RST_CNT_EN = 0x1U;  /* Disable CMP0 and reset on event  */
+    CT_IEP.TMR_CMP_CFG_bit.CMP_EN = 0x1U;
+    CT_IEP.TMR_GLB_CFG_bit.DEFAULT_INC = 0x1U;      /* Configure incr value             */
+    CT_IEP.TMR_GLB_CFG_bit.CNT_EN = 1U;             /* Enable counter                   */
 }
