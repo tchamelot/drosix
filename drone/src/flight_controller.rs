tchamelot/drosix

commit 950264c8ec46736085dfbd9e62b8eda80a646489

[DRONE] Update dependencies
Switch to mpu9250 upstream because it does not change much and it is
easier for maintainability

drone/src/flight_controller.rs
@@ -54,8 +54,8 @@ impl<'a> FlightController {
         controller.set_rate_pid(config.rate_pid);
         controller.set_rate_pid(config.attitude_pid);
         controller.switch_debug(config.debug_config);
-        PruController::start(&mut pru.pru0, &mut pru.pru1)?;
 
+        PruController::start(&mut pru.pru0, &mut pru.pru1)?;
         let start = Instant::now();
 
         'control_loop: loop {

drone/src/sensor.rs
@@ -1,10 +1,10 @@
-use std::fs::File;
+use std::fs;
 use std::io::Read;
 use std::os::unix::io::{AsRawFd, RawFd};
 
 use anyhow::{Context, Error, Result};
 
-use gpio_cdev::{Chip, EventRequestFlags, LineEventHandle, LineRequestFlags};
+use hal::gpio_cdev::{Chip, EventRequestFlags, LineEventHandle, LineRequestFlags};
 use hal::Delay;
 use hal::I2cdev;
 
@@ -20,24 +20,29 @@ pub struct Sensors {
 }
 
 impl Sensors {
+    /// Initiates all the sensors:
+    /// - IMU: MPU9250
     pub fn new() -> Result<Self> {
         let i2c = I2cdev::new("/dev/i2c-2").context("Opening i2c bus")?;
-        let mut dmp_firmware: Vec<u8> = Vec::new();
-        File::open("/lib/firmware/mpu_firmware.bin")
-            .unwrap()
-            .read_to_end(&mut dmp_firmware)
-            .context("Reading mpu9250 firmware")?;
 
-        let mut chip = Chip::new("/dev/gpiochip3").context("Opening GPIO")?;
+        let mut mpu_config = MpuConfig::dmp();
+        mpu_config
+            .dmp_rate(DmpRate::_100Hz)
+            .dmp_features_raw_gyro(true)
+            .dmp_features_raw_accel(true)
+            .dmp_features_quat6(true)
+            .dmp_features_gyro_auto_calibrate(true);
+
+        let dmp_firmware = fs::read("/lib/firmware/mpu_firmware.bin")?;
+
         // 117 : gpiochip3 => 3*32 = 96. 117 - 96 = 21
-        let pin = chip.get_line(21).context("Accessing IMU interrupt pin")?;
-        let pin_event = pin
-            .events(LineRequestFlags::INPUT, EventRequestFlags::FALLING_EDGE, "mpu9250")
+        let pin_event = Chip::new("/dev/gpiochip3")
+            .and_then(|mut chip| chip.get_line(21))
+            .and_then(|line| line.events(LineRequestFlags::INPUT, EventRequestFlags::FALLING_EDGE, "mpu9250"))
             .context("Registering IMU interrupt")?;
 
-        let mut mpu9250 = MpuConfig::dmp().dmp_rate(DmpRate::_100Hz).build(i2c);
-        mpu9250.init(&mut Delay, &dmp_firmware).map_err(|_| Error::msg("Statring mpu9250"))?;
-        mpu9250.reset_fifo(&mut Delay).map_err(|_| Error::msg("Clearing mpu9250 data"))?;
+        let mpu9250 = Mpu9250::dmp(i2c, &mut Delay, &mut mpu_config, &dmp_firmware)
+            .map_err(|e| Error::msg(format!("Statring mpu9250 {:?}", e)))?;
 
         Ok(Self {
             imu: mpu9250,
@@ -51,11 +56,11 @@ impl Sensors {
 
     pub fn handle_imu_event(&mut self) -> Result<Odometry> {
         self.imu_pin.get_event().context("Accessing IMU interrupt pin")?;
-        match self.imu.dmp_all() {
+        match self.imu.dmp_all::<[f32; 3], [f64; 4]>() {
             Ok(measure) => {
-                let attitude = quat_to_angles(&measure.quaternion);
-                let gyro = measure.gyro;
-                let thrust = compute_thrust(&measure.accel, &attitude);
+                let attitude = quat_to_angles(&measure.quaternion.unwrap());
+                let gyro = measure.gyro.unwrap();
+                let thrust = compute_thrust(&measure.accel.unwrap(), &attitude);
                 Ok(Odometry {
                     attitude,
                     rate: Angles {
@@ -70,9 +75,9 @@ impl Sensors {
         }
     }
 
+    /// Reset the IMU internal state keeping the same config
     pub fn clean_imu(&mut self) -> Result<()> {
-        self.imu.reset_fifo(&mut Delay).map_err(|_| Error::msg("Reseting IMU communication"))?;
-        Ok(())
+        self.imu.reset_fifo(&mut Delay).map_err(|e| Error::msg(format!("Reseting IMU communication {:?}", e)))
     }
 }
 

Cargo.toml
@@ -1,5 +1,7 @@
 [workspace]
 
+resolver = "2"
+
 members = [
     "drone",
     "model",

drone/Cargo.toml
@@ -11,9 +11,9 @@ default-target = "armv7-unknown-linux-gnueabihf"
 [dependencies]
 # Flight controller
 hal                 = { version = "0.3", package = "linux-embedded-hal" }
-mio                 = { version = "0.7", features = ["os-poll", "os-ext", "net"] } 
+mio                 = { version = "0.8", features = ["os-poll", "os-ext"] } 
+mpu9250             = { version = "0.25", features = ["i2c", "dmp"] }
 prusst              = { git = "https://github.com/tchamelot/prusst" }
-gpio-cdev           = "0.5"
 
 # Parameters
 serde               = { version = "1.0", features = ["derive"] }
@@ -23,10 +23,6 @@ toml                = "0.8"
 gilrs = "0.10"
 
 anyhow              = "1.0"
-[dependencies.mpu9250]
-git         = "https://github.com/tchamelot/mpu9250"
-branch      = "drosix"
-features    = ["i2c", "dmp_quat6", "dmp_accel", "dmp_gyro", "dmp_motion"]
 
 [build-dependencies]
 cbindgen = "0.26"

commit ea278781bf05c22c5a6c9634b7f89cf3466a8593

[DRONE] Fix interrupt issue with MPU9250
Bug introduced in 950264c.
Hardware bug impose tap feature to have interrupt slower than 200Hz

drone/src/flight_controller.rs
@@ -74,7 +74,8 @@ impl<'a> FlightController {
                     IMU => {
                         self.fly(&mut sensors, &mut controller).or_else(|err| match err.downcast_ref::<Error>() {
                             // TODO Handle error as critical if the drone is armed or flying
-                            Some(Error::NotAvailable | Error::NotCalibarated) => Ok(()),
+                            Some(Error::NotCalibarated) => Ok(()),
+                            Some(Error::NotAvailable) => Ok(log::warn!("IMU data not available")),
                             _ => Err(err),
                         })?
                     },

drone/src/sensor.rs
@@ -51,6 +51,7 @@ impl Sensors {
             .dmp_features_raw_gyro(true)
             .dmp_features_raw_accel(true)
             .dmp_features_quat6(true)
+            .dmp_features_tap(true)
             .dmp_features_gyro_auto_calibrate(true);
 
         let dmp_firmware = fs::read("/lib/firmware/mpu_firmware.bin")?;