a09f0fdb8bebf989ed4512fb6b76195fb11fe3df
T. Chamelot <chamelot.thomas@gmail.com>
2024-10-26 10:53:49 +0200

    [PRU] Update test for new PID

--- pru/tests/pid.py
+++ pru/tests/pid.py
@@ -6,33 +6,145 @@ from utils import load
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
-SOURCES = [BASE_DIR + '/../src/pid.c']
-INCLUDE_PATH = ['.', BASE_DIR + '/../src']
+SOURCES = [BASE_DIR + "/../src/pid.c"]
+INCLUDE_PATH = [".", BASE_DIR + "/../src"]
 
-module, ffi = load(SOURCES, INCLUDE_PATH, ['-std=c99'])
+module, ffi = load(SOURCES, INCLUDE_PATH, ["-std=c99"])
 ffi.dlopen(None)
 
-
-KP = 10.0
-TI = 1.0
-TD = 1.0
+KP = 2541.0
+TI = 1.75
+TD = 41.52
+N = 5
 T = 0.01
+MAX = 20000.0
+MIN = -MAX
 
 
-class TestPid(unittest.TestCase):
+def gen_aw_pid(kp, ti, td, n, t, umax=1e10, kaw=0):
+    def sat_update(t, x, u, params):
+        return []
+
+    def sat_output(t, x, u, params):
+        return np.clip(u, -umax, umax)
+
+    sat = ct.nlsys(
+        sat_update, sat_output, name="sat", inputs=1, outputs=1, states=0, dt=t
+    )
+
+    proportional = ct.tf([kp], [1]).sample(t, method="bilinear", name="p")
+    integrative = ct.tf([kp], [ti, 0]).sample(t, method="bilinear", name="i")
+    derivative = ct.tf([kp * td, 0], [td / n, 1]).sample(t, method="bilinear", name="d")
+    pid_sum = ct.tf([1], [1], name="sum", dt=t)
+    aw_sum = ct.tf([1], [1, 0], name="aw", dt=t)
+    return ct.interconnect(
+        [proportional, integrative, derivative, pid_sum, sat, aw_sum],
+        connections=[
+            ["i.u", "aw.y"],
+            ["aw.u", ("sum", "y", -kaw), ("sat", "y", kaw)],
+            ["sum.u", "p.y", "i.y", "d.y"],
+            ["sat.u", "sum.y"],
+        ],
+        inplist=[["p.u", "i.u", "d.u"]],
+        outlist=[["sat.y"], ["sum.y"]],
+        inputs=["error"],
+        outputs=["y", "y_int"],
+        name="pid",
+        check_unused=False,
+    )
+
+
+def pid_setup(kp, ti, td, n, t, minimum=-1e10, maximum=1e10, kaw=0):
+    pid = ffi.new("struct pid_controller_t*")
+    pid_config = ffi.new("pid_config_t*")
+    pid_config.kpr = kp
+    pid_config.ti = ti
+    pid_config.td = td
+    pid_config.filter = n
+    pid_config.max = maximum
+    pid_config.min = minimum
+    pid_config.kaw = kaw
+
+    module.pid_init(pid, pid_config, t)
+
+    return pid
+
+
+class ProportionalOnly(unittest.TestCase):
     def setUp(self):
-        self.t = 0.0 + np.arange(0, 10) * T
-        self.ct_pid = (KP + ct.tf([KP / TI], [1, 0]) + ct.tf([KP * TD, 0], [TD / 5, 1])).sample(T, 'bilinear')
-        self.ffi_pid = ffi.new('struct pid_t*')
-        module.pid_init(self.ffi_pid,
-                        self.ct_pid.num[0][0].tolist(),
-                        self.ct_pid.den[0][0][1:].tolist())
+        self.pid = pid_setup(KP, 0, 0, 0, T)
 
-    def testPid(self):
-        _, ct_y = ct.step_response(self.ct_pid, self.t)
-        ffi_y = [module.pid_run(self.ffi_pid, 1.0) for _ in self.t]
-        np.testing.assert_allclose(ffi_y, ct_y, rtol=1e-5)
+    def test_coeff_p(self):
+        np.testing.assert_allclose(self.pid.kd[0], KP, rtol=1e-5)
+        np.testing.assert_allclose(self.pid.kd[1], 0, rtol=1e-5)
+        np.testing.assert_allclose(self.pid.kd[2], 0, rtol=1e-5)
+        np.testing.assert_allclose(self.pid.ki, 0, rtol=1e-5)
+
+    def test_output(self):
+        cmd = np.sin(np.arange(0, 50))
+        out = [module.pid_run(self.pid, x) for x in cmd]
+        np.testing.assert_allclose(out, KP * cmd, rtol=1e-5)
 
 
-if __name__ == '__main__':
+class SimplePid(unittest.TestCase):
+    def setUp(self):
+        self.pid = pid_setup(KP, TI, TD, N, T)
+        self.i = ct.tf([KP], [TI, 0]).sample(T, method="bilinear")
+        self.pd = KP * (1 + ct.tf([TD, 0], [TD / N, 1])).sample(T, method="bilinear")
+
+    def test_coeff_i(self):
+        np.testing.assert_allclose(self.pid.ki, self.i.num[0][0], rtol=1e-5)
+
+    def test_coeff_pd(self):
+        np.testing.assert_allclose(self.pid.kd[2], self.pd.den[0][0][1], rtol=1e-5)
+        np.testing.assert_allclose(self.pid.kd[0], self.pd.num[0][0][0], rtol=1e-5)
+        np.testing.assert_allclose(self.pid.kd[1], self.pd.num[0][0][1], rtol=1e-5)
+
+    def test_output(self):
+        time = np.arange(0, 50) * T
+        ref_pid = self.i + self.pd
+        out = [module.pid_run(self.pid, 1.0) for _ in time]
+        ref = ct.step_response(ref_pid, time)
+        np.testing.assert_allclose(out, ref.outputs, rtol=1e-5)
+
+
+class SaturationPid(unittest.TestCase):
+    def setUp(self):
+        self.pid = pid_setup(KP, TI, TD, N, T, minimum=MIN, maximum=MAX)
+        self.ref_pid = gen_aw_pid(KP, TI, TD, N, T, umax=MAX)
+        self.time = np.arange(0, 100) * T
+
+    def test_sat_max(self):
+        step = 1.312
+        ref = ct.input_output_response(self.ref_pid, T=self.time, U=step)
+        out = [module.pid_run(self.pid, step) for _ in self.time]
+        np.testing.assert_allclose(out, ref.outputs[0], rtol=1e-3)
+
+    def test_sat_min(self):
+        step = -1.312
+        ref = ct.input_output_response(self.ref_pid, T=self.time, U=step)
+        out = [module.pid_run(self.pid, step) for _ in self.time]
+        np.testing.assert_allclose(out, ref.outputs[0], rtol=1e-3)
+
+    def test_sat_both(self):
+        step = np.sin(self.time * 2 * np.pi * 5) * 1.4
+        ref = ct.input_output_response(self.ref_pid, T=self.time, U=step)
+        out = [module.pid_run(self.pid, x) for x in step]
+        np.testing.assert_allclose(out, ref.outputs[0], rtol=1e-3, atol=1)
+
+
+class AntiWindupPid(unittest.TestCase):
+    def setUp(self):
+        self.pid = pid_setup(KP, TI, TD, N, T, minimum=MIN, maximum=MAX, kaw=1)
+        self.ref_pid = gen_aw_pid(KP, TI, TD, N, T, umax=MAX, kaw=1)
+        self.time = np.arange(0, 50) * T
+
+    def test_output(self):
+        step = np.sin(self.time * 2 * np.pi * 5) * 1.4
+        ref = ct.input_output_response(self.ref_pid, T=self.time, U=step)
+        out = [module.pid_run(self.pid, x) for x in step]
+        np.testing.assert_allclose(out, ref.outputs[0], rtol=5e-2, atol=1)
+
+
+if __name__ == "__main__":
     unittest.main()
--- pru/tests/utils.py
+++ pru/tests/utils.py
@@ -26,63 +26,79 @@ import pycparser.c_generator
 import re
 import subprocess
 import uuid
+import os
+import glob
 
 Remove_Unknowns = """\
-        #define __attribute__(x)
+#define __attribute__(x)
 #define __restrict
 #define __asm__(x)
 #define __extension__
 """
 
 
-def load(source_files,
-         include_paths=[],
-         compiler_options=[],
-         remove_unknowns='',
-         module_name='pysim_',
-         avoid_cache=False,
-         en_code_coverage=False,
-         en_sanitize_undefined=False,
-         en_sanitize_address=False):
+def load(
+    source_files,
+    include_paths=[],
+    compiler_options=[],
+    remove_unknowns="",
+    module_name="pysim_",
+    avoid_cache=False,
+    en_code_coverage=False,
+    en_sanitize_undefined=False,
+    en_sanitize_address=False,
+):
     """Load a C file into Python as a module.
 
-source_files:     ['file1.c', file2.c'] or just 'file1.c'
-include_paths:    ['.', './includes']
-compiler_options: ['-std=c90', '-O0', '-Wall', '-Wextra']
+    source_files:     ['file1.c', file2.c'] or just 'file1.c'
+    include_paths:    ['.', './includes']
+    compiler_options: ['-std=c90', '-O0', '-Wall', '-Wextra']
 
-module_name: sets the module name (and names of created files).
+    module_name: sets the module name (and names of created files).
 
-avoid_cache=True: makes random names to allow testing code with global variables.
+    avoid_cache=True: makes random names to allow testing code with global variables.
 
-en_code_coverage=True: enables Gcov code coverage.
+    en_code_coverage=True: enables Gcov code coverage.
 
-en_sanitize_undefined=True: enables undefined behavior sanitizer.
+    en_sanitize_undefined=True: enables undefined behavior sanitizer.
 
-en_sanitize_address=True: enables address sanitizer (not working).
-"""
-    # Avoid caching using random name to module
-    if avoid_cache:
-        module_name += uuid.uuid4().hex + '_'
-
+    en_sanitize_address=True: enables address sanitizer (not working).
+    """
     # Create a list if just one souce file in a string
-    if type(source_files) == str:
+    if isinstance(source_files, str):
         source_files = [source_files]
 
+    # Avoid caching using random name to module
+    if avoid_cache:
+        module_name += uuid.uuid4().hex + "_"
+    else:
+        header_files = []
+        for path in include_paths:
+            path = os.path.normpath(path)
+            header_files.extend(glob.glob(path + "/*.h"))
+        module = get_cached_module(module_name, source_files + header_files)
+        if module is not None:
+            return module.lib, module.ffi
+
     # Prepend -I on include paths
-    include_paths = ['-I' + x for x in include_paths]
+    include_paths = ["-I" + x for x in include_paths]
 
     # Collect source code
     source_content = []
     for file in source_files:
         with open(file) as fp:
             source_content.append(fp.read())
-    source_content = '\n'.join(source_content)
+    source_content = "\n".join(source_content)
 
     # Collect include files
     # TODO Remove inclusions inside comments
-    header_content = ''.join(x[0] for x in re.findall(
-        r'(\s*\#\s*(include\s|define\s|undef\s|if(n?def)?\s|else|endif|error|warning)[^\n\r\\]*((\\\n|\\\r|\\\n\r|\\\r\n)[^\n\r\\]*)*)',
-        source_content))
+    header_content = "".join(
+        x[0]
+        for x in re.findall(
+            r"(\s*\#\s*(include\s|define\s|undef\s|if(n?def)?\s|else|endif|error|warning)[^\n\r\\]*((\\\n|\\\r|\\\n\r|\\\r\n)[^\n\r\\]*)*)",
+            source_content,
+        )
+    )
 
     # Preprocess include files
     header_content = Remove_Unknowns + remove_unknowns + header_content
@@ -93,20 +109,22 @@ en_sanitize_address=True: enables address sanitizer (not working).
     source_content = preprocess(source_content, include_paths, compiler_options)
 
     # Remove conflicts
-    header_content = header_content.replace('typedef struct { int __val[2]; } __fsid_t;', '')
-    source_content = source_content.replace('typedef struct { int __val[2]; } __fsid_t;', '')
+    header_content = header_content.replace(
+        "typedef struct { int __val[2]; } __fsid_t;", ""
+    )
+    source_content = source_content.replace(
+        "typedef struct { int __val[2]; } __fsid_t;", ""
+    )
 
     # Rename main, to avoid any conflicts, and declare it in the header
-    if 'int main(' in source_content:
+    if "int main(" in source_content:
         # TODO REGEX
-        source_content = source_content.replace('int main(', 'int mpmain(')
-        header_content += '\nint mpmain(int argc, char **argv);\n'
-    elif 'void main(' in source_content:
+        source_content = source_content.replace("int main(", "int mpmain(")
+        header_content += "\nint mpmain(int argc, char **argv);\n"
+    elif "void main(" in source_content:
         # TODO REGEX
-        source_content = source_content.replace('void main(', 'void mpmain(')
-        header_content += '\nvoid mpmain(void argc, char **argv);\n'
-
-    # TODO Compile only if source_content is different than module_name.c
+        source_content = source_content.replace("void main(", "void mpmain(")
+        header_content += "\nvoid mpmain(void argc, char **argv);\n"
 
     # Prepend 'extern "Python+C" ' to functions declarations with no definitions
     # try:
@@ -126,7 +144,7 @@ en_sanitize_address=True: enables address sanitizer (not working).
     # Run CFFI
     ffibuilder = cffi.FFI()
     ffibuilder.cdef(header_content)
-    include_dirs = [x.replace('-I', '') for x in include_paths]
+    include_dirs = [x.replace("-I", "") for x in include_paths]
 
     extra_compile_args = []
     extra_link_args = []
@@ -135,24 +153,35 @@ en_sanitize_address=True: enables address sanitizer (not working).
     if en_sanitize_address:
         # Address sanitizer
         # export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libasan.so.4
-        extra_compile_args += ['-fsanitize=address']
-        libraries += ['asan']
+        extra_compile_args += ["-fsanitize=address"]
+        libraries += ["asan"]
 
     if en_sanitize_undefined:
-        extra_compile_args += ['-fsanitize=undefined', ]
-        extra_link_args += ['-fsanitize=undefined', ]
+        extra_compile_args += [
+            "-fsanitize=undefined",
+        ]
+        extra_link_args += [
+            "-fsanitize=undefined",
+        ]
 
     if en_code_coverage:
         # Code coverage
-        extra_compile_args += ['--coverage', ]
-        extra_link_args += ['--coverage', ]
+        extra_compile_args += [
+            "--coverage",
+        ]
+        extra_link_args += [
+            "--coverage",
+        ]
         libraries += []
 
-    ffibuilder.set_source(module_name, source_content,
-                          include_dirs=include_dirs,
-                          extra_compile_args=extra_compile_args,
-                          libraries=libraries,
-                          extra_link_args=extra_link_args)
+    ffibuilder.set_source(
+        module_name,
+        source_content,
+        include_dirs=include_dirs,
+        extra_compile_args=extra_compile_args,
+        libraries=libraries,
+        extra_link_args=extra_link_args,
+    )
     ffibuilder.compile()
 
     # Import and return resulting module
@@ -164,16 +193,37 @@ en_sanitize_address=True: enables address sanitizer (not working).
 
 def preprocess(source, include_paths, compiler_options):
     try:
-        command = ['gcc', ] + compiler_options + include_paths + ['-E', '-P', '-']
+        command = (
+            [
+                "gcc",
+            ]
+            + compiler_options
+            + include_paths
+            + ["-E", "-P", "-"]
+        )
         return subprocess.check_output(command, input=source, universal_newlines=True)
     except:
         print()
-        print(80 * '-')
-        print('SOURCE/HEADER:')
+        print(80 * "-")
+        print("SOURCE/HEADER:")
         print(source)
-        print(80 * '-')
+        print(80 * "-")
         print()
         raise
+
+
+def get_cached_module(module_name: str, sources: list[str]):
+    try:
+        module = importlib.import_module(module_name)
+    except ModuleNotFoundError:
+        return None
+
+    cache_time = os.path.getmtime(module.__file__)
+    for source in sources:
+        if os.path.getmtime(source) > cache_time:
+            return None
+
+    return module
 
 
 class HeaderGenerator(pycparser.c_generator.CGenerator):
@@ -191,8 +241,18 @@ class HeaderGenerator(pycparser.c_generator.CGenerator):
             if n.name in self.functions:
                 # Is already in functions
                 return result
-            elif re.search((re.escape(result).replace('\\*', '\\*\\s*').replace('\\ ', '\\s*') + '\\s*\{'),
-                           self.source_content) is not None:
+            elif (
+                re.search(
+                    (
+                        re.escape(result)
+                        .replace("\\*", "\\*\\s*")
+                        .replace("\\ ", "\\s*")
+                        + "\\s*\{"
+                    ),
+                    self.source_content,
+                )
+                is not None
+            ):
                 # Is declared in source content
                 return result
             else:
@@ -205,34 +265,37 @@ class HeaderGenerator(pycparser.c_generator.CGenerator):
 
     def visit_FuncDef(self, n, *args, **kwargs):
         self.functions.add(n.decl.name)
-        return ''
+        return ""
 
 
 class FFIMocks:
     """Define 'extern "Pyton+C"' function as a mock object.
 
-from unit_test_c_with_python import load, FFIMocks
-module, ffi = load('gpio.c')
+    from unit_test_c_with_python import load, FFIMocks
+    module, ffi = load('gpio.c')
 
-Mocks = FFIMocks()
+    Mocks = FFIMocks()
 
-# Create mocks
-Mocks.CreateMock(ffi, 'read_gpio0', return_value=42)
-Mocks.CreateMock(ffi, 'read_gpio1', return_value=21)
+    # Create mocks
+    Mocks.CreateMock(ffi, 'read_gpio0', return_value=42)
+    Mocks.CreateMock(ffi, 'read_gpio1', return_value=21)
 
-# Reset mocks [Useful in setUp()]
-Mocks.ResetMocks()
-"""
+    # Reset mocks [Useful in setUp()]
+    Mocks.ResetMocks()
+    """
+
     from unittest.mock import call
 
     def CreateMock(self, ffi, name, *args, **kwargs):
         import unittest.mock
+
         mock = unittest.mock.Mock(*args, **kwargs)
         setattr(self, name, mock)
         ffi.def_extern(name)(mock)
 
     def ResetMocks(self):
         import unittest.mock
+
         for name in dir(self):
             obj = getattr(self, name)
             if isinstance(obj, unittest.mock.Mock):
